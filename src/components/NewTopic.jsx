49925f9570f14b5b8a9d5ac3fd9ee9574d724cf9
Izam Ghali <izamghali18@gmail.com>
2023-12-24 13:14:06 +0700

    UPDATED: TopicPage with illustration and NewTopic button

--- src/components/NewTopic.jsx
+++ src/components/NewTopic.jsx
@@ -1,9 +1,28 @@
 import React from "react"
 
-export default function NewTopic(props) {
+export default function NewTopic({ topics }) {
     return (
         <div>
-            <h1>Hello New Topic!</h1>
+            <a className="flex items-center gap-2 group " href="" >
+                <svg className="w-5 h-5 border-[1px] rounded-full border-stone-300 fill-stone-300 
+                    duration-300 group-hover:border-stone-950 
+                    group-hover:bg-stone-950 group-hover:fill-stone-50
+                " xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                </svg>
+                { topics ? 
+                    <span className="border-b-[1.5px] border-stone-300 border-dotted text-stone-600
+                    duration-300 group-hover:text-stone-950 group-hover:border-stone-950 group-hover:border-solid">
+                       Create a new topic
+                   </span>
+                : 
+                    <span className="border-b-[1.5px] border-stone-300 border-dotted text-stone-800
+                        duration-300 group-hover:text-stone-950 group-hover:border-stone-950 group-hover:border-solid">
+                        Create a new topic
+                    </span>
+                }
+                
+            </a>
         </div>
     )
 };
--- src/pages/TopicPage.jsx
+++ src/pages/TopicPage.jsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import NewTopic from "../components/NewTopic"
+import noDataImg from '../assets/no-data.png'
 
 export default function TopicPage(props) {
+
+    const [ topics, setTopics ] = useState(false) // FIX:  This should be array, not boolean
+
     return (
-        <section>
-            <h1>This is topic page</h1>
-            {/* <NewTopic/> */}
+        <section className="flex flex-col items-center gap-20">
+            { topics ? 
+                    <h1 className="">There's data</h1> // FIX:  Loop topics here          
+                : 
+                    <div className="flex flex-col items-center gap-4">
+                        <img className="w-48 sm:w-80" src={noDataImg} />
+                        <span className="text-stone-500">~ There is no topic yet 🥲 ~</span>
+                    </div>
+                    
+            }
+            <NewTopic topics={topics}/>
+            
         </section>
     )
 };
