4dae5542c111fe71dcd9d9b93533bab7a6a4e651
Izam Ghali <izamghali18@gmail.com>
2023-12-26 23:22:04 +0700

    UPDATED: GridListButton, passing props from TopicPage to TopicCard to transform card later on

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -8,7 +8,7 @@ export default function TopicCard({ topics }) {
                 <h2 className="font-bold text-2xl sm:text-3xl lg:text-4xl mb-8">Available Topics</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8">
                     { topics.map((topic, index) => {
-                        return <div className="border-2 border-black rounded-lg h-auto w-full p-4 flex flex-col justify-between sm:hover:-translate-y-4 duration-300" key={index}>
+                        return <div className="border-2 border-black rounded-lg h-auto w-full p-4 flex flex-col justify-between sm:hover:-translate-y-4 duration-300 cursor-pointer" key={index}>
                             <div className="">
                                 <h3 className="text-lg sm:text-xl lg:text-2xl font-semibold text-stone-900">{topic.title}</h3>
                                 <p className="text-stone-600">{topic.desc}</p>
--- src/pages/TopicPage.jsx
+++ src/pages/TopicPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import noDataImg from '../assets/no-data.png'
 import NewButton from "../components/NewButton"
 import TopicCard from "../components/TopicCard"
+import GridListButton from "../components/GridListButton"
 
 export default function TopicPage({ page }) {
 
@@ -33,11 +34,15 @@ export default function TopicPage({ page }) {
     ]
 
     const [ topics, setTopics ] = useState(mockupTopics)
+    const [ view, setView ] = useState(false)
 
     return (
         <section className="flex flex-col gap-6 w-full lg:max-w-[70rem] px-8">
 
-            <div className="hidden sm:block"><NewButton page={page}/></div>
+            <div className="flex justify-between">
+                <div className="hidden sm:block"><NewButton page={page}/></div>
+                <GridListButton view={view} setView={setView}/>
+            </div>
 
             { topics.length > 0 ? 
                     <TopicCard topics={topics} />
