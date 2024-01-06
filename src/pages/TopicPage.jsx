d5446bb91f154ab1d32c2078bd2908afdaafff19
Izam Ghali <izamghali18@gmail.com>
2024-01-06 23:40:54 +0700

    UPDATED: openEditOption function on TopicPage

--- src/pages/TopicPage.jsx
+++ src/pages/TopicPage.jsx
@@ -36,6 +36,18 @@ export default function TopicPage({ page }) {
     const [ topics, setTopics ] = useState(mockupTopics)
     const [ view, setView ] = useState(false)
 
+    function openEditOption(event) {
+        const removeOption = event.target.parentElement.nextSibling
+        removeOption.classList.toggle('translate-y-10')
+    }
+
+    function dismissRemoveOption(event) {
+        // const removeModal = document.getElementById('remove-modal')
+        // removeModal.classList.remove('translate-y-0')
+        // removeModal.classList.add('translate-y-10')
+        // console.log(event);
+    }
+
 
     return (
         <section className="flex flex-col gap-6 w-full lg:max-w-[70rem] px-8">
@@ -52,32 +64,44 @@ export default function TopicPage({ page }) {
                     <GridListButton view={view} setView={setView}/>
 
                     {/* Open the modal using document.getElementById('ID').showModal() method */}
-                    <dialog id="my_modal_1" className="modal">
-                    <div className="modal-box">
-                        <h3 className="font-bold text-lg">Manage topics</h3>
+                    <dialog onClick={dismissRemoveOption} id="my_modal_1" className="modal">
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg">Manage topics</h3>
 
-                        {/* FIX:  Add remove button here */}
+                            {/* FIX:  Add remove button here */}
 
-                        <div className="space-y-2 ">
-                            { topics.map((topic, index) => {
-                                return <div className="group border-2 border-slate-500 rounded-md p-2 overflow-hidden">
-                                        <div className="flex justify-between">
-                                            <h4>{topic.title}</h4>
-                                            <svg className="w-5 cursor-pointer group-hover:translate-x-0 translate-x-10 duration-200" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                                                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
-                                            </svg>
+                            {/* list container */}
+                            <div className="space-y-2 ">
+                                { topics.map((topic, index) => {
+                                    return <div className="group border-2 border-slate-500 rounded-md p-2 overflow-hidden relative" key={index}>
+                                            <div className="flex justify-between">
+                                                <h4>{topic.title}</h4>
+                                                <svg onClick={openEditOption} className="w-5 cursor-pointer group-hover:translate-x-0 translate-x-10 duration-200" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
+                                                </svg>
+                                            </div>
+
+                                            <div id="remove-modal" className="absolute top-0 bottom-0 right-0 flex items-center translate-y-10 duration-200
+                                                bg-slate-100 text-slate-700 p-2 rounded-md
+                                            ">
+                                                <div className="flex flex-col p-2">
+                                                    <span>edit</span>
+                                                    <span>remove</span>
+
+                                                </div>
+                                            </div>
+
                                         </div>
-                                    </div>
-                            }) }
+                                }) }
+                            </div>
+                            
+                            <div className="modal-action">
+                                <form method="dialog">
+                                    {/* if there is a button in form, it will close the modal */}
+                                    <button className="btn">Save</button>
+                                </form>
+                            </div>
                         </div>
-                        
-                        <div className="modal-action">
-                            <form method="dialog">
-                                {/* if there is a button in form, it will close the modal */}
-                                <button className="btn">Save</button>
-                            </form>
-                        </div>
-                    </div>
                     </dialog>
 
                 </div>
