925f876f9d2ed21996fd9179e72ac6185d966dc7
Izam Ghali <izamghali18@gmail.com>
2024-03-09 10:43:17 +0700

    COMPONENT & MOCK: created TopicTitle component & populate mock up data on Card component and styled it with grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,27 +2,52 @@ import React from "react"
 
 export default function Card(props) {
 
+    const testCard = [
+        { front: 'Batman', back: 'Bruce Wayne' },
+        { front: 'Superman', back: 'Clark Kent' },
+        { front: 'Flash', back: 'Barry Allen' },
+        { front: 'Wonder Woman', back: 'Princess Diana' },
+        { front: 'Green Lantern', back: 'Hal Jordan' },
+        { front: 'Aquaman', back: 'Arthur Curry' },
+        // { front: 'Aquaman', back: 'Arthur Curry' },
+    ]
+
     function handleClick(event) {
         
     }
 
     return (
-        <section>
+        <section className="
+            grid gap-10
+            grid-cols-1
+            sm:grid-cols-2
+            lg:grid-cols-3
+            xl:grid-cols-4
+        
+            border-2 border-violet-400 w-[90%]">
 
-            <div className={`
-                card w-96 h-40 bg-base-100 shadow-xl 
-                overflow-hidden
-                duration-200 group
-                `} 
-                onClick={handleClick}
-                >
-                    <div className="group-hover:hidden bg-base-200 w-full h-full flex justify-center items-center">
-                        <p className="">Front card</p>
+            {
+                testCard.map((card, idx) => {
+                    return <div className={`
+                        card w-auto h-40 
+                        bg-base-100 shadow-xl 
+                        overflow-hidden
+                        duration-200 group
+                        `} 
+                        onClick={handleClick}
+                        key={idx}
+                        >
+                            <div className="group-hover:hidden bg-base-200 w-full h-full flex justify-center items-center">
+                                <p className="">{card.front}</p>
+                            </div>
+                            <div className="group-hover:flex hidden bg-emerald-200 w-full h-full justify-center items-center">
+                                <p className="">{card.back}</p>
+                            </div>
                     </div>
-                    <div className="group-hover:flex hidden bg-emerald-200 w-full h-full justify-center items-center">
-                        <p className="">Back card</p>
-                    </div>
-            </div>
+                    
+                })
+            }
+
 
 
             
---
+++ src/components/TopicTitle.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function TopicTitle(props) {
+    return (
+        <div>
+            <h1 className="text-5xl font-semibold"><span className="font-extralight">Topic:</span> Hello World!</h1>
+        </div>
+    )
+};
+
